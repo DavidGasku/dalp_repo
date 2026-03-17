0120f5e6b1f031747268e4b4de0d8adc1b012b59
David Gascueña Ferré <davidgasku@gmail.com>
2026-03-17 18:04:36 +0100

    http request

--- programa.py
+++ programa.py
@@ -1,9 +1,14 @@
 '''
 Mi programa
 '''
+import requests
 
 major_version = 0
 minor_version = 5
 patch_version = 1
 
 print(f"Version {major_version}.{minor_version}.{patch_version}")
+
+respuesta = requests.get("https://httpbin.org/get")
+print(f"Status: {respuesta.status_code}")
+print(f"IP: {respuesta.json()['origin']}")
